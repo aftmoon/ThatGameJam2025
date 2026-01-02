0729f827f49ad124594365fbb4b30701168c06ae
aftmoon <ask_forthemoon@163.com>
2026-01-03 02:38:09 +0800

    通过场景转换连接各个场景
    加入开始界面，球球连接加上交叉判断逻辑，在用ui的基础上重写了拼图代码，场景转换

---
+++ Assets/Scripts/ScriptableObject/DayDataSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Data/Day Data")]
+public class DayDataSO : ScriptableObject
+{
+    public int currentDay = 0;
+}
